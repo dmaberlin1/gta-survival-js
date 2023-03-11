756dd5b8711109759a2cfce478c5c18f34884e56
dmaberlin1 <dmaberlin77@gmail.com>
2023-03-11 22:55:53 +0200

    feat: class Player, canvas methods

---
+++ player.js
@@ -0,0 +1,43 @@
+export class Player {
+    constructor(x,y,context) {
+        this.x=x;
+        this.y=y;
+        this.context=context;
+        this.cursorPosition={
+            x:0,
+            y:0
+        };
+        document.addEventListener('mousemove',event=>{
+            this.cursorPosition.x=event.clientX;
+            this.cursorPosition.y=event.clientY;
+        })
+
+        this.image=new Image()
+        this.image.src='./img/player.png'
+        this.imageWidth=50;
+        this.imageHeight=60;
+    }
+    drawImg(){
+        this.context.drawImage(
+          this.image,
+          0,
+          0,
+          this.imageWidth,
+          this.imageHeight,
+          this.x-this.imageWidth/2,
+          this.y-this.imageHeight/2,
+          this.imageWidth,
+          this.imageHeight
+        )
+    }
+    
+    draw(){
+        this.context.save();
+        let angle=Math.atan2(this.cursorPosition.y-this.y,this.cursorPosition.x-this.x);
+        this.context.translate(this.x,this.y);
+        this.context.rotate(angle+Math.PI/2);
+        this.context.translate(-this.x,-this.y);
+        this.drawImg();
+        this.context.restore()
+    }
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+import {Player} from "./player.js";
+
+const canvas=document.querySelector('canvas');
+const context=canvas.getContext('2d');
+canvas.width=document.documentElement.clientWidth;
+canvas.height=document.documentElement.clientHeight;
+
+let player=new Player(canvas.width/2,canvas.height/2,context)
+
+player.drawImg()
+
+animate()
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    player.draw();
+}
